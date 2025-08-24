a6ac1d6c8bdbe002f6804ff95f9efd692faf3a4d
karat <karateev.92@gmail.com>
2025-08-24 20:44:48 +0400

    feat: регистрация моделей Entry, Tag и Mood в админке

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Entry, Tag, Mood
 
-# Register your models here.
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_at", "mood")
+    list_filter = ("mood", "created_at")
+    search_fields = ("title", "text")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Mood)
+class MoodAdmin(admin.ModelAdmin):
+    list_display = ("emoji", "name")
+    search_fields = ("name",)
