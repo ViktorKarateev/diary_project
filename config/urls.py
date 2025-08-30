a3677e001749426ea422c133223c567e07c838ff
karat <karateev.92@gmail.com>
2025-08-30 19:23:07 +0400

    feat: добавлен шаблон формы входа login.html

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('api/', include('diary.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
