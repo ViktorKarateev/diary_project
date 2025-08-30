a232e1633fc0c87e5bae5a61134ed44c7826102e
karat <karateev.92@gmail.com>
2025-08-30 18:52:07 +0400

    feat: добавлен эндпоинт /tag-subscriptions для управления подписками

--- diary/urls.py
+++ diary/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .views import EntryViewSet, TagViewSet, MoodViewSet, UserProfileView
+from .views import EntryViewSet, TagViewSet, MoodViewSet, UserProfileView, TagSubscriptionViewSet
 
 router = DefaultRouter()
 router.register(r'entries', EntryViewSet)
 router.register(r'tags', TagViewSet)
 router.register(r'moods', MoodViewSet)
+router.register(r'tag-subscriptions', TagSubscriptionViewSet, basename='tag-subscriptions')
+
 
 urlpatterns = [
     path('', include(router.urls)),
