16984de6d246853de5b81bbd6677c0aed7b06b1b
karat <karateev.92@gmail.com>
2025-08-30 21:06:31 +0400

    добавлен докерфаил и докер-компос,docs: добавлен шаблон переменных окружения .env.example

--- config/settings.py
+++ config/settings.py
@@ -8,15 +8,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q=429s!pk(f^e+3qgt%=55loojklw1m398@d+ur1%byykl!dmv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
