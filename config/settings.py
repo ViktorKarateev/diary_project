b6397b5369ca8e50e1c87ba53c169d98590fc162
karat <karateev.92@gmail.com>
2025-08-30 19:13:48 +0400

    feat: подключён шаблонизатор и базовый шаблон base.html с Bootstrap

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'diary' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
