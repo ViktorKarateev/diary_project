a62d011f9dbee67764d31739fff44da3b28f1699
karat <karateev.92@gmail.com>
2025-09-03 05:40:01 +0400

    diary: почистить импорты; исправить get_success_url; защитить детальную запись и выровнять success_url

--- diary/views.py
+++ diary/views.py
@@ -7,10 +7,8 @@ from .permissions import IsOwnerOrReadOnly
 from rest_framework.generics import RetrieveUpdateAPIView
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView,UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
@@ -77,16 +75,19 @@ class TagSubscriptionViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-@method_decorator(login_required, name='dispatch')
-class EntryCreateView(CreateView):
+class EntryCreateView(LoginRequiredMixin, CreateView):
     model = Entry
     fields = ['title', 'text', 'mood', 'tags']
     template_name = 'diary/entry_form.html'
-    success_url = reverse_lazy('entry_create')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        # БЫЛА ОШИБКА: не хватало закрывающей скобки )
+        return reverse("diary:entry_detail", kwargs={"pk": self.object.pk})
+
 
 
 class EntryListView(LoginRequiredMixin, ListView):
@@ -109,27 +110,29 @@ class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = []
 
-
 class EntryUpdateView(LoginRequiredMixin, UpdateView):
-    model = Entry
-    fields = ['title', 'text', 'mood', 'tags']
-    template_name = 'diary/entry_form.html'
-    success_url = reverse_lazy('entry_list')
+        model = Entry
+        fields = ['title', 'text', 'mood', 'tags']
+        template_name = 'diary/entry_form.html'
+        success_url = reverse_lazy('diary:entry_list')  # было 'entry_list'
 
-    def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user)
-
+        def get_queryset(self):
+            return Entry.objects.filter(user=self.request.user)
 
 class EntryDeleteView(LoginRequiredMixin, DeleteView):
-    model = Entry
-    template_name = 'diary/entry_confirm_delete.html'
-    success_url = reverse_lazy('entry_list')
+        model = Entry
+        template_name = 'diary/entry_confirm_delete.html'
+        success_url = reverse_lazy('diary:entry_list')  # было 'entry_list'
 
-    def get_queryset(self):
-        return Entry.objects.filter(user=self.request.user)
+        def get_queryset(self):
+            return Entry.objects.filter(user=self.request.user)
 
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LoginRequiredMixin, DetailView):
     model = Entry
     template_name = "diary/entry_detail.html"
     context_object_name = "entry"
+
+    def get_queryset(self):
+        # чтобы нельзя было смотреть чужие записи по URL
+        return Entry.objects.filter(user=self.request.user)
